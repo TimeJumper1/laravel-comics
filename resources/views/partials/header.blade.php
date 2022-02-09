a762a5ff9048197d1836ba367642ca9839ce1c33
TimeJumper1 <benassai.stefano.1@gmail.com>
2022-02-09 17:02:44 +0100

    scafholding base

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,18 @@
+<header>
+    <div class="header-container">
+        <img src="{{ asset('images/logo-dc.png') }}" alt="logo" class="logo-header">
+        <ul class="header-list">
+            <li>characthers</li>
+            <li class="active">comics</li>
+            <li>characthers</li>
+            <li>characthers</li>
+            <li>characthers</li>
+            <li>characthers</li>
+            <li>characthers</li>
+            <li>characthers</li>
+            <li>characthers</li>
+            <li>characthers</li>
+        </ul>
+    </div>
+
+</header>
